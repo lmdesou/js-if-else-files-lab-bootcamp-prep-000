aa7d0a31e7cd21d28fc46771f6a47e717b7821d2
Lalita Desouza <lmdesou@yahoo.com>
2017-10-19 19:29:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ if (name === "Susan") {
 }
 
 if (height === "70") {
-    console.log("The name is correct")
+    console.log("The height is correct")
 } else {
     console.log("Expected the name to be Susan")
 }
